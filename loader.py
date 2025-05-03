1ca9db8c58540e7f1686baec20eae832e21a8902
Conordeveloper0084 <conordev0084@gmail.com>
2025-05-03 09:30:22 +0500

    O'zgartirishlar kiritildi last

--- loader.py
+++ loader.py
@@ -7,9 +7,6 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 load_dotenv()  # .env faylni avtomatik yuklaydi
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-if not BOT_TOKEN:
-    raise ValueError("BOT_TOKEN topilmadi! .env faylda BOT_TOKEN ni tekshiring.")
-
 bot = Bot(token=BOT_TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
