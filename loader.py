afddeac2d09fbc9efcf30f178e89f912681700e9
Conordeveloper0084 <conordev0084@gmail.com>
2025-05-03 08:23:55 +0500

    O'zgartirishlar kiritildi

--- loader.py
+++ loader.py
@@ -1,8 +1,7 @@
-from aiogram import Bot, Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from dotenv import load_dotenv
-from config import BOT_TOKEN
 import os
+from dotenv import load_dotenv
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram import Bot, Dispatcher
 
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
