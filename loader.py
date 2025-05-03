2d0f836a0628790ca86ee27c11da52d9f78a10ef
Conordeveloper0084 <conordev0084@gmail.com>
2025-05-03 09:02:20 +0500

    O'zgartirishlar kiritildi last

--- loader.py
+++ loader.py
@@ -1,13 +1,11 @@
+# loader.py
 import os
 from dotenv import load_dotenv
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram import Bot, Dispatcher
-
-load_dotenv()
-BOT_TOKEN = os.getenv('BOT_TOKEN')
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+load_dotenv(dotenv_path="/full/path/to/.env")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 bot = Bot(token=BOT_TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
-
-# print("BOT_TOKEN is:", BOT_TOKEN)
