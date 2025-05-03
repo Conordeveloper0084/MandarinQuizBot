587346f9366c41b7bfabb57ad7a21793863637c6
Conordeveloper0084 <conordev0084@gmail.com>
2025-05-03 07:23:22 +0500

    Updated app.py to integrate Flask and Aiogram, added start.sh

---
+++ app.py
@@ -0,0 +1,28 @@
+import os
+import threading
+from flask import Flask
+from aiogram import executor
+from loader import dp  
+import handlers 
+
+# Flask ilovasi
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, World!"
+
+def start_flask():
+    """Flask ilovasini ishga tushirish"""
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
+def start_bot():
+    """Aiogram botini ishga tushirish"""
+    executor.start_polling(dp, skip_updates=True)
+
+if __name__ == "__main__":
+    # Flask va botni alohida thread-larda ishga tushirish
+    flask_thread = threading.Thread(target=start_flask)
+    flask_thread.start()
+
+    start_bot()
